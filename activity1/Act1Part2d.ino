08736fbf1585471d097c31b0385dc899c7dc89f0
chizbreds <gabbydaphne.rodriguez@proton.me>
2026-06-13 19:21:13 +0800

    Added 4 new files.

---
+++ activity1/Act1Part2d.ino
@@ -0,0 +1,88 @@
+#include <Arduino.h>
+#include "api/Common.h"
+
+const int ledCount = 7;
+const int blinkDelay = 500;
+
+int ledArray[ledCount] = {8, 7, 6, 5, 4, 3, 2};
+
+bool digit_0[ledCount] = {LOW, LOW, LOW, LOW, LOW, LOW, HIGH};
+bool digit_1[ledCount] = {HIGH, LOW, LOW, HIGH, HIGH, HIGH, HIGH,};
+bool digit_2[ledCount] = {LOW, LOW, HIGH, LOW, LOW, HIGH, LOW};
+bool digit_3[ledCount] = {LOW, LOW, LOW, LOW, HIGH, HIGH, LOW};
+bool digit_4[ledCount] = {HIGH, LOW, LOW, HIGH, HIGH, LOW, LOW};
+bool digit_5[ledCount] = {LOW, HIGH, LOW, LOW, HIGH, LOW, LOW};
+bool digit_6[ledCount] = {LOW, HIGH, LOW, LOW, LOW, LOW, LOW};
+bool digit_7[ledCount] = {LOW, LOW, LOW, HIGH, HIGH, HIGH, HIGH};
+bool digit_8[ledCount] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW};
+bool digit_9[ledCount] = {LOW, LOW, LOW, LOW, HIGH, LOW, LOW};
+bool digit_a[ledCount] = {LOW, LOW, LOW, HIGH, LOW, LOW, LOW};
+bool digit_b[ledCount] = {HIGH, HIGH, LOW, LOW, LOW, LOW, LOW};
+bool digit_c[ledCount] = {HIGH, HIGH, HIGH, LOW, LOW, HIGH, LOW};
+bool digit_d[ledCount] = {HIGH, LOW, LOW, LOW, LOW, HIGH, LOW};
+bool digit_e[ledCount] = {LOW, HIGH, HIGH, LOW, LOW, LOW, LOW};
+bool digit_f[ledCount] = {LOW, HIGH, HIGH, HIGH, LOW, LOW, LOW};
+bool digit_l[ledCount] = {HIGH, HIGH, HIGH, LOW, LOW, LOW, HIGH};
+bool digit_v[ledCount] = {HIGH, LOW, LOW, LOW, LOW, LOW, HIGH};
+bool digit_y[ledCount] = {HIGH, LOW, LOW, LOW, HIGH, LOW, LOW};
+bool digit_h[ledCount] = {HIGH, LOW, LOW, HIGH, LOW, LOW, LOW};
+
+void clearDisplay() {
+  for (int i = 0; i <= ledCount; i++) {
+    digitalWrite(ledArray[i], HIGH);
+
+  }
+
+}
+
+void showDigit(bool digit[ledCount]) {
+
+  for (int i = 0; i < ledCount; i++) {
+    digitalWrite(ledArray[i], digit[i]);
+
+  }
+
+}
+
+void setup() {
+
+  for (int i = 0; i < ledCount; i++) {
+    pinMode(ledArray[i], OUTPUT);
+
+  }
+
+}
+
+void loop() {
+  showDigit(digit_h);
+  delay(blinkDelay);
+
+  clearDisplay();
+  delay(blinkDelay);
+
+  showDigit(digit_e);
+  delay(blinkDelay);
+
+  clearDisplay();
+  delay(blinkDelay);
+   
+  showDigit(digit_l);
+  delay(blinkDelay);
+
+  clearDisplay();
+  delay(blinkDelay);
+
+  showDigit(digit_l);
+  delay(blinkDelay);
+
+  clearDisplay();
+  delay(blinkDelay);
+
+  showDigit(digit_0);
+  delay(blinkDelay);
+
+  clearDisplay();
+  delay(blinkDelay);
+
+}
+
